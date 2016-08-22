fcfcef83e816c4f3d1c2f53e03034d8e66152376
EC2 Default User <ec2-user@ip-10-0-0-117.us-west-2.compute.internal>
2016-08-22 15:00:46 +0000

    added many more versions of the server

--- server_v4.py
+++ server_v4.py
@@ -3,6 +3,7 @@ import sys
 import datetime
 import sys,os
 import cherrypy
+import hashlib
 
 import smtplib
 import email
@@ -48,6 +49,50 @@ def get_HNews_info(user_name):
     parser = MyHTMLParser()
     html_resp = parser.feed(proc_output)
     return parser #return a HTML parser
+
+class Register(object):
+    @cherrypy.expose
+    def index(self):
+        return """<html>
+<head><title>open</title>
+</head>
+<body>
+
+<form id="ping_form" target="console_iframe" method="post" action="register">
+  username: <br><br>
+  <input type="text" id="username" name="username" size="18" /><br><br>
+  password: <br><br>
+  <input type="text" id="password" name="password" size="18" /> <br><br>
+  <button id="ping" class="fg-button ui-state-default ui-corner-all" type="submit">
+  Register
+  </button>
+  </form>
+
+</body>
+        </html>"""
+    @cherrypy.expose
+    def register(self, username, password):
+        secrets_file=open("/home/ec2-user/secrets.txt")
+
+        passwords=secrets_file.read().rstrip('\n')
+
+        db_password = passwords.split('\n')[0]
+
+        dbname = "open"
+
+        h = hashlib.sha256()
+
+        h.update(password)
+
+        conn = MySQLdb.connect(host='tutorial-db-instance.cphov5mfizlt.us-west-2.rds.amazonaws.com', user='open', passwd=db_password, port=3306)
+
+        curs = conn.cursor()
+
+        curs.execute("use "+dbname+";")
+
+        curs.execute("insert into user_info set username = \""+username+"\", hashed_password = \""+h.hexdigest()+"\"")
+
+        conn.commit()
 
 class ShowMessages(object):
     @cherrypy.expose
@@ -162,24 +207,112 @@ class Chat(object):
     def index(self):
         return """<html>
 <head><title>open</title>
+
+<style>
+    .fg-button {
+    outline: 0;
+    clear: left;
+    margin:0 4px 0 0;
+    padding: .1em .5em;
+    text-decoration:none !important;
+    cursor:pointer;
+    position: relative;
+    text-align: center;
+    zoom: 1;
+    }
+    .fg-button .ui-icon {
+    position: absolute;
+    top: 50%;
+    margin-top: -8px;
+    left: 50%;
+    margin-left: -8px;
+    }
+    a.fg-button { float:left;  }
+    .terminal {
+    position: relative;
+    top: 0;
+    left: 0;
+    display: block;
+    font-family: monospace;
+    white-space: pre;
+    width: 100%; height: 30em;
+    border: none;
+    }
+</style>
+
 </head>
 <body>
 
-<form id="ping_form" target="console_iframe" method="post" action="chat">
+<form id="show_messages_form" target="console_iframe1" method="post" action="chat">
   your username: <br><br>
   <input type="text" id="username1" name="username1" size="18" /><br><br>
   username of the user that you want to chat with: <br><br>
   <input type="text" id="username2" name="username2" size="18" /> <br><br>
-  <button id="ping" class="fg-button ui-state-default ui-corner-all" type="submit">
+  <button id="chat" class="fg-button ui-state-default ui-corner-all" type="submit">
   Display Messages
   </button>
   </form>
 
+ <form id="add_message_form" target="console_iframe2" method="post" action="add_message">
+  <input type="text" id="message" name="message" size="18" />
+  <button id="chat" class="fg-button ui-state-default ui-corner-all" type="submit">
+  Chat
+  </button>
+  </form>
+
+  <iframe name="console_iframe1" class="terminal" />  
+  <iframe name="console_iframe2" class="terminal" />  
+
+
 </body>
         </html>"""
 
+
+    @cherrypy.expose
+    def add_message(self, message):
+
+
+        username1 = cherrypy.session.get("username1")
+        username2 = cherrypy.session.get("username2")
+        forward= cherrypy.session.get("forward")
+
+        secrets_file=open("/home/ec2-user/secrets.txt")
+
+        passwords=secrets_file.read().rstrip('\n')
+
+        db_password = passwords.split('\n')[0]
+
+        dbname = "open"
+
+        conn = MySQLdb.connect(host='tutorial-db-instance.cphov5mfizlt.us-west-2.rds.amazonaws.com', user='open', passwd=db_password, port=3306)
+
+        curs = conn.cursor()
+
+        curs.execute("use "+dbname+";")
+            
+        curs.execute("insert into messages set username1 = \""+username1+"\", username2 = \""+username2+"\", forward="+forward+", time=now(6), message = \""+message+"\";")
+
+        conn.commit()
+
     @cherrypy.expose
     def chat(self, username1, username2):
+
+        sorted_usernames= sorted([username1,username2])
+
+        if sorted_usernames == [username1,username2]:
+            forward=str(1)
+        else:
+            forward=str(0)
+
+        username1 = sorted_usernames[0]
+        username2 = sorted_usernames[1]
+
+        cherrypy.session['username1'] = username1
+        cherrypy.session['username2'] = username2
+        cherrypy.session['forward'] = forward
+
+        cherrypy.session.save()
+
         #dn=cherrypy.request.headers['Cms-Authn-Dn']
 
         secrets_file=open("/home/ec2-user/secrets.txt")
@@ -192,18 +325,10 @@ class Chat(object):
 
         #conn = MySQLdb.connect(host=' tutorial-db-instance.cphov5mfizlt.us-west-2.rds.amazonaws.com:3306', user='open', passwd="openserver")
 
-        return_message=""
+        def print_messages():
 
-        #return_message = str(messages)
-
-        print "andrew debug 1"
-
-        def print_messages_2():
-
-
-
-
-            print "andrew debug 2"
+            #for some reason, if there no messages inserted already, without this yield statement, the session does not get saved
+            yield "<br>"
 
             conn = MySQLdb.connect(host='tutorial-db-instance.cphov5mfizlt.us-west-2.rds.amazonaws.com', user='open', passwd=db_password, port=3306)
 
@@ -216,13 +341,7 @@ class Chat(object):
 
             current_time=datetime.datetime.now()
 
-            print username1
-
-            print username2
-
             curs.execute("select * from messages where username1 = \""+username1+"\" and username2 = \""+username2+"\" order by time;")
-
-            print "andrew debug 3"
 
             colnames = [desc[0] for desc in curs.description]
 
@@ -230,20 +349,15 @@ class Chat(object):
 
             for message in messages:
 
-                print "andrew debug 4"
-
                 message_dict=dict(zip(colnames, message))
 
                 if message_dict["forward"] == 1:
-                    print "andrew debug 4.1"
-                    yield str(message_dict["username1"] +": " + message_dict["message"]+"<br>");
+                    yield str(message_dict["username1"] +": " + message_dict["message"]+"<br>")
                 elif message_dict["forward"] == 0:
-                    print "andrew debug 4.2"
-                    yield str(message_dict["username2"] + ": " + message_dict["message"]+"<br>");
-
-            print "andrew debug 5"
+                    yield str(message_dict["username2"] + ": " + message_dict["message"]+"<br>")
 
             while True:
+
 
                 time.sleep(1)
 
@@ -256,10 +370,10 @@ class Chat(object):
                 current_time_old=current_time
                 current_time=datetime.datetime.now()
 
+                print current_time_old.strftime("%y:%m:%d %H:%M:%S.%f")
+
                 #curs.execute("select * from messages where username1 = \""+username1+"\" and username2 = \""+username2+"\" order by time;")
-                curs.execute("select * from messages where username1 = \""+username1+"\" and username2 = \""+username2+"\" and time > \""+current_time_old.strftime("%y:%m:%d %H:%M:%S.%u")+"\" order by time;")
-
-
+                curs.execute("select * from messages where username1 = \""+username1+"\" and username2 = \""+username2+"\" and time > \""+current_time_old.strftime("%y:%m:%d %H:%M:%S.%f")+"\" order by time;")
 
                 colnames = [desc[0] for desc in curs.description]
 
@@ -281,7 +395,9 @@ class Chat(object):
                     elif message_dict["forward"] == 0:
                         yield str(message_dict["username2"] + ": " + message_dict["message"]+"<br>");
 
-        return print_messages_2()
+        #return
+
+        return print_messages()
 
     chat._cp_config = {'response.stream': True}
 
@@ -289,8 +405,10 @@ class Chat(object):
 if __name__ == '__main__':
     cherrypy.config.update({'server.socket_port': 8080})
     cherrypy.config.update({'server.socket_host': 'ec2-52-42-148-78.us-west-2.compute.amazonaws.com'})
-    #cherrypy.tree.mount(ShowMessages(),'/show_messages')
+    cherrypy.config.update({'tools.sessions.on': True})
+    cherrypy.tree.mount(ShowMessages(),'/show_messages')
     cherrypy.tree.mount(Chat(),'/chat')
+    cherrypy.tree.mount(Register(),'/register')
     #cherrypy.quickstart(Open())
 
     cherrypy.engine.start()
