c62a24d2863938977118f9ef617065e51b5dbca3
EC2 Default User <ec2-user@ip-10-0-0-117.us-west-2.compute.internal>
2017-09-28 21:40:45 +0000

    no commit message

--- server.py
+++ server.py
@@ -63,7 +63,7 @@ def require(*conditions):
 about_html_string = """
             Ecommunicate is intended to meet the need for electronic communication that is the opposite of private. Making online communication public instead of private could lead to increased levels of honesty, transparency, cooperation and decreased levels of deception, misunderstanding between people, and duplicative work. It is not clear whether this concept will catch on, but it is worth a try.<br><br>
 
-           We hope to eventually provide chat, e-mail, and audio/video calling services, but currently only chat, similar to Google Hangout or WeChat, is implemented. You can chat yourself (after registering and logging in) or you can view other people's conversations. This website is experimental at this point. You should expect bugs, unexpected downtime, etc. Please contact feedback@ecommunicate.ch for comments, feature requests, etc. <br><br>
+          E-mail (like Gmail) and text messaging (like Google Hangouts or WeChat) are implemented already, and we hope to eventually add audio and video calling (like Skype). You can chat or e-mail yourself (after registering and logging in) or you can view other people's chat conversations or e-mail inboxes (see below). This website is experimental at this point. You should expect bugs, unexpected downtime, etc. Please contact feedback@ecommunicate.ch for comments, feature requests, etc. <br><br>
 
   Below is a list of all of the services that we would like to provide. The ones that are operational are in bold.
 
@@ -87,13 +87,13 @@ about_html_string = """
 <ol>
 <li>Create
 <ol>
-<li>Browser
+<li><b>Browser</b>
 <li>Android
 <li>iOS
 </ol>
 <li>View
 <ol>
-<li>Browser
+<li><b>Browser</b>
 <li>Android
 <li>iOS
 </ol>
@@ -381,7 +381,7 @@ li.menubar {
 
 </center>
 
-      Ecommunicate is a free online communication service in which all communication is viewable by anyone on the open internet instead of being private. Currently, only text messaging, similar to Google Hangout or WeChat, is implemented. You can chat yourself (after registering and logging in) or you can view other people's conversations. This website is experimental at this point. You should expect bugs, unexpected downtime, etc. Please contact feedback@ecommunicate.ch for comments, feature requests, etc.<br> <br>
+      Ecommunicate is a free online communication service in which all communication is viewable by anyone on the open internet instead of being private. E-mail (like Gmail) and text messaging (like Google Hangouts or WeChat) are implemented already, and we hope to eventually add audio and video calling (like Skype). You can chat or e-mail yourself (after registering and logging in) or you can view other people's chat conversations or e-mail inboxes (see below). This website is experimental at this point. You should expect bugs, unexpected downtime, etc. Please contact feedback@ecommunicate.ch for comments, feature requests, etc.<br> <br>
 
       Register here for your free account. Please remember your username and password, as there is no way to recover them at this time.
 
@@ -1255,7 +1255,7 @@ li.menubar {
 </center>
 
 """
-            html_string = html_string+"Ecommunicate is a free online communication service in which all communication is viewable by anyone on the open internet instead of being private. Currently, only text messaging, similar to Google Hangout or WeChat, is implemented. You can chat yourself (after registering and logging in) or you can view other people's conversations (see below). This website is experimental at this point. You should expect bugs, unexpected downtime, etc. Please contact feedback@ecommunicate.ch for comments, feature requests, etc.<br>"
+            html_string = html_string+"      Ecommunicate is a free online communication service in which all communication is viewable by anyone on the open internet instead of being private. E-mail (like Gmail) and text messaging (like Google Hangouts or WeChat) are implemented already, and we hope to eventually add audio and video calling (like Skype). You can chat or e-mail yourself (after registering and logging in) or you can view other people's chat conversations or e-mail inboxes (see below). This website is experimental at this point. You should expect bugs, unexpected downtime, etc. Please contact feedback@ecommunicate.ch for comments, feature requests, etc.<br>"
 
             html_string=html_string+"<br>"
 
@@ -1370,7 +1370,7 @@ li.menubar {
 
 """
 
-            html_string = html_string+"Ecommunicate is a free online communication service in which all communication is viewable by anyone on the open internet instead of being private. Currently, only text messaging, similar to Google Hangout or WeChat, is implemented. You can chat yourself (after registering and logging in) or you can view other people's conversations (see below). This website is experimental at this point. You should expect bugs, unexpected downtime, etc. Please contact feedback@ecommunicate.ch for comments, feature requests, etc.<br>"
+            html_string = html_string+"      Ecommunicate is a free online communication service in which all communication is viewable by anyone on the open internet instead of being private. E-mail (like Gmail) and text messaging (like Google Hangouts or WeChat) are implemented already, and we hope to eventually add audio and video calling (like Skype). You can chat or e-mail yourself (after registering and logging in) or you can view other people's chat conversations or e-mail inboxes (see below). This website is experimental at this point. You should expect bugs, unexpected downtime, etc. Please contact feedback@ecommunicate.ch for comments, feature requests, etc.<br>"
 
             html_string=html_string+"<br>"
 
@@ -2292,7 +2292,7 @@ li.menubar {
 </center>
 
 """
-            html_string = html_string+"Ecommunicate is a free online communication service in which all communication is viewable by anyone on the open internet instead of being private. Currently, only text messaging, similar to Google Hangout or WeChat, is implemented. You can chat yourself (after registering and logging in) or you can view other people's conversations (see below). This website is experimental at this point. You should expect bugs, unexpected downtime, etc. Please contact feedback@ecommunicate.ch for comments, feature requests, etc.<br>"
+            html_string = html_string+"Ecommunicate is a free online communication service in which all communication is viewable by anyone on the open internet instead of being private. E-mail (like Gmail) and text messaging (like Google Hangouts or WeChat) are implemented already, and we hope to eventually add audio and video calling (like Skype). You can chat or e-mail yourself (after registering and logging in) or you can view other people's chat conversations or e-mail inboxes (see below). This website is experimental at this point. You should expect bugs, unexpected downtime, etc. Please contact feedback@ecommunicate.ch for comments, feature requests, etc.<br>"
 
             html_string=html_string+"<br>"
 
@@ -2451,7 +2451,7 @@ def is_right_password(username, password):
 if __name__ == '__main__':
     cherrypy.config.update({'server.socket_port': 443}) #port 443 for https or port 80 for http
 #    cherrypy.config.update({'server.socket_port': 80})
-    cherrypy.config.update({'server.socket_host': 'ec2-52-42-148-78.us-west-2.compute.amazonaws.com'})
+    cherrypy.config.update({'server.socket_host': 'ec2-35-163-111-83.us-west-2.compute.amazonaws.com'})
     #cherrypy.config.update({'tools.sessions.on': True})
     #cherrypy.config.update({'tools.auth.on': True})
 
