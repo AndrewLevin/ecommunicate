783b4e386e09e9366dca0d5c32c8d66f85786caf
EC2 Default User <ec2-user@ip-10-0-0-117.us-west-2.compute.internal>
2017-10-20 18:17:52 +0000

    no commit message

--- about.py
+++ about.py
@@ -11,9 +11,7 @@ class About(object):
 
         issessionauthenticated = utils.is_session_authenticated()
 
-        if not issessionauthenticated:
-
-            html_string = """
+        html_string = """
 <html>
 <head>
 <title>
@@ -29,38 +27,11 @@ li.menubar {
 <body>
 <center><h1>Ecommunicate</h1>
 <h3>A free online communication service</h3>
-"""+html_strings.not_authenticated_menubar_html_string+"""
+"""+(html_strings.authenticated_menubar_html_string if utils.is_session_authenticated() else html_strings.not_authenticated_menubar_html_string)+"""
 <h4>About This Website</h4>
 </center>
 """+html_strings.about_html_string+"""
 </body>
 </html>
 """
-
-        else:
-
-            html_string = """
-<html>
-<head>
-<title>
-Ecommunicate
-</title>
-<style>
-li.menubar {
-        display: inline;
-        padding: 20px;
-}
-</style>
-</head>
-<body>
-<center><h1>Ecommunicate</h1>
-<h3>A free online communication service</h3>
-"""+html_strings.authenticated_menubar_html_string+"""
-<h4>About This Website</h4>
-</center>
-"""+html_strings.about_html_string+"""
-</body>
-</html>
-"""
-
         return html_string
