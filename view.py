198bbab6396557e69f259c12ff32b15d63c29113
EC2 Default User <ec2-user@ip-10-0-0-117.us-west-2.compute.internal>
2017-10-20 18:25:51 +0000

    no commit message

--- view.py
+++ view.py
@@ -36,86 +36,86 @@ li.menubar {
 </head>
 <body>
 """
-
-            html_string = html_string+"<center><h1>Ecommunicate</h1>"
-            html_string = html_string+"<h3>A free online communication service</h3>"
-            html_string = html_string+(html_strings.authenticated_menubar_html_string if utils.is_session_authenticated() else html_strings.not_authenticated_menubar_html_string)+"""
+        
+        html_string = html_string+"<center><h1>Ecommunicate</h1>"
+        html_string = html_string+"<h3>A free online communication service</h3>"
+        html_string = html_string+(html_strings.authenticated_menubar_html_string if utils.is_session_authenticated() else html_strings.not_authenticated_menubar_html_string)+"""
 <h4>View</h4>
 </center>
 """
-            html_string = html_string+"      Ecommunicate is a free online communication service in which all communication is viewable by anyone on the open internet instead of being private. E-mail to other ecommunicate.ch e-mail addresses and text messaging (like Google Hangouts or WeChat) are implemented already, and we hope to eventually add audio and video calling (like Skype). You can chat or e-mail yourself (after registering and logging in) or you can view other people's chat conversations or e-mail inboxes (see below). This website is experimental at this point. You should expect bugs, unexpected downtime, etc. Please contact ecommunicate.feedback@gmail.com for comments, feature requests, etc.<br>"
+        html_string = html_string+"      Ecommunicate is a free online communication service in which all communication is viewable by anyone on the open internet instead of being private. E-mail to other ecommunicate.ch e-mail addresses and text messaging (like Google Hangouts or WeChat) are implemented already, and we hope to eventually add audio and video calling (like Skype). You can chat or e-mail yourself (after registering and logging in) or you can view other people's chat conversations or e-mail inboxes (see below). This website is experimental at this point. You should expect bugs, unexpected downtime, etc. Please contact ecommunicate.feedback@gmail.com for comments, feature requests, etc.<br>"
 
-            html_string=html_string+"<br>"
+        html_string=html_string+"<br>"
+        
+        html_string = html_string + "<table>"
+    
+        html_string = html_string + "<tr><th>Chat Conversations</th><th>E-mail Boxes</th></tr>"
+    
+        html_string = html_string + "<tr>\n"
 
-            html_string = html_string + "<table>"
+        html_string = html_string + "<td valign=\"top\">\n"
 
-            html_string = html_string + "<tr><th>Chat Conversations</th><th>E-mail Boxes</th></tr>"
+        html_string=html_string+"<ol>\n"
 
-            html_string = html_string + "<tr>\n"
+        secrets_file=open("/home/ec2-user/secrets.txt")
 
-            html_string = html_string + "<td valign=\"top\">\n"
+        passwords=secrets_file.read().rstrip('\n')
 
-            html_string=html_string+"<ol>\n"
+        db_password = passwords.split('\n')[0]
 
-            secrets_file=open("/home/ec2-user/secrets.txt")
+        dbname = "ecommunicate"
 
-            passwords=secrets_file.read().rstrip('\n')
+        conn = MySQLdb.connect(host='ecommunicate-production.cphov5mfizlt.us-west-2.rds.amazonaws.com', user='browser', passwd=db_password, port=3306)
 
-            db_password = passwords.split('\n')[0]
+        curs = conn.cursor()
 
-            dbname = "ecommunicate"
+        curs.execute("use "+dbname+";")
 
-            conn = MySQLdb.connect(host='ecommunicate-production.cphov5mfizlt.us-west-2.rds.amazonaws.com', user='browser', passwd=db_password, port=3306)
+        curs.execute("select DISTINCT username1,username2 from messages;")
 
-            curs = conn.cursor()
+        conversations = curs.fetchall()
 
-            curs.execute("use "+dbname+";")
+        colnames = [desc[0] for desc in curs.description]
 
-            curs.execute("select DISTINCT username1,username2 from messages;")
+        for conversation in conversations:
 
-            conversations = curs.fetchall()
-
-            colnames = [desc[0] for desc in curs.description]
-
-            for conversation in conversations:
-
-                conversation_dict=dict(zip(colnames, conversation))
+            conversation_dict=dict(zip(colnames, conversation))
             
-                html_string=html_string+"<li><a href=\"/view/chat/?username1=%22"+conversation_dict["username1"]+"%22&username2=%22"+conversation_dict["username2"]+"%22\">"+conversation_dict["username1"]+" and "+conversation_dict["username2"]+"</a><br></li>\n"
+            html_string=html_string+"<li><a href=\"/view/chat/?username1=%22"+conversation_dict["username1"]+"%22&username2=%22"+conversation_dict["username2"]+"%22\">"+conversation_dict["username1"]+" and "+conversation_dict["username2"]+"</a><br></li>\n"
 
-            html_string=html_string+"</ol>\n"
+        html_string=html_string+"</ol>\n"
 
-            html_string = html_string +"</td>\n"
+        html_string = html_string +"</td>\n"
+        
+        html_string = html_string +"<td valign=\"top\">\n"
 
-            html_string = html_string +"<td valign=\"top\">\n"
+        curs.execute("select username from user_info;")
+        
+        colnames = [desc[0] for desc in curs.description]
 
-            curs.execute("select username from user_info;")
+        usernames = curs.fetchall()
+        
+        html_string=html_string+"<ol>\n"
+        
+        for username in usernames:
 
-            colnames = [desc[0] for desc in curs.description]
+            username_dict=dict(zip(colnames, username))
 
-            usernames = curs.fetchall()
+            html_string=html_string+"<li><a href=\"/view/email?username=%22"+username_dict["username"]+"%22\">"+username_dict["username"]+"</a><br></li>\n"
 
-            html_string=html_string+"<ol>\n"
+        html_string=html_string+"</ol>\n"
 
-            for username in usernames:
+        html_string = html_string +"</td>\n"
 
-                username_dict=dict(zip(colnames, username))
-
-                html_string=html_string+"<li><a href=\"/view/email?username=%22"+username_dict["username"]+"%22\">"+username_dict["username"]+"</a><br></li>\n"
-
-            html_string=html_string+"</ol>\n"
-
-            html_string = html_string +"</td>\n"
-
-            html_string = html_string + "</tr>\n"
-
-            html_string = html_string + "</table>"
+        html_string = html_string + "</tr>\n"
+            
+        html_string = html_string + "</table>"
 
 
-            html_string = html_string+"""
+        html_string = html_string+"""
 </body>
 </html>
 """
-            conn.close()
+        conn.close()
 
-            return html_string
+        return html_string
