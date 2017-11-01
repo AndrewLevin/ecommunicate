c9de08dbb9ea18408b3f2d0c006625a35827c072
EC2 Default User <ec2-user@ip-10-0-0-117.us-west-2.compute.internal>
2017-11-01 10:10:50 +0000

    no commit message

--- readone.py
+++ readone.py
@@ -38,26 +38,26 @@ class ReadOne(object):
 
         email_string = ""
 
-        email_string = email_string+"<table border = \"1\" width = \"100%\"  >"
+        email_string += "<table border = \"1\" width = \"100%\"  >"
 
         if sent_bool:
             if 'To' in em:
                 if email.utils.parseaddr(em['To'])[0]:
-                    email_string = email_string + "<tr><td><b>To: </b>"+email.utils.parseaddr(em['To'])[0]+"</td></tr>"
+                    email_string += "<tr><td><b>To: </b>"+email.utils.parseaddr(em['To'])[0]+"</td></tr>"
                 else:    
-                    email_string = email_string + "<tr><td><b>To: </b>"+email.utils.parseaddr(em['To'])[1]+"</td></tr>"
+                    email_string += "<tr><td><b>To: </b>"+email.utils.parseaddr(em['To'])[1]+"</td></tr>"
             #email_string = email_string + email.utils.parseaddr(em[1]['From'])[1]+"<br>"            
         else:
             if 'From' in em:
                 if email.utils.parseaddr(em['From'])[0]:
-                    email_string = email_string + "<tr><td><b>From: </b>"+email.utils.parseaddr(em['From'])[0]+"</td></tr>"
+                    email_string += "<tr><td><b>From: </b>"+email.utils.parseaddr(em['From'])[0]+"</td></tr>"
                 else:
-                    email_string = email_string + "<tr><td><b>From: </b>"+email.utils.parseaddr(em['From'])[1]+"</td></tr>"
+                    email_string += "<tr><td><b>From: </b>"+email.utils.parseaddr(em['From'])[1]+"</td></tr>"
                 #email_string = email_string + email.utils.parseaddr(em[1]['From'])[1]+"<br>"
         if 'Subject' in em:    
-            email_string = email_string + "<tr><td><b>Subject: </b>"+em['Subject']+"</td></tr>"
+            email_string += "<tr><td><b>Subject: </b>"+em['Subject']+"</td></tr>"
         if 'Date' in em:
-            email_string = email_string + "<tr><td><b>Date: </b>"+time.strftime("%d %b %H:%M",email.utils.parsedate(em['Date']))+"</td></tr>"
+            email_string += "<tr><td><b>Date: </b>"+time.strftime("%d %b %H:%M",email.utils.parsedate(em['Date']))+"</td></tr>"
 
         attachment_string = ""
 
@@ -70,32 +70,32 @@ class ReadOne(object):
 
                     if 'X-Attachment-Id' in payload and "Content-Description" in payload:
 
-                        attachment_string = attachment_string + '<tr><td><a href="/email/readone/attachment/?message_id='+message_id+'&&attachment_id='+payload["X-Attachment-Id"]+'">'+payload["Content-Description"]+'</a></tr></td>'
+                        attachment_string += '<tr><td><a href="/email/readone/attachment/?message_id='+message_id+'&&attachment_id='+payload["X-Attachment-Id"]+'">'+payload["Content-Description"]+'</a></tr></td>'
 
                     else:
                         if 'Content-Type' in payload and ('text/plain' in payload['Content-Type'] or 'message/delivery-status' in payload['Content-Type'] or 'message/rfc822' in payload['Content-Type']):
-                            body_string = body_string + "<tr><td>"+payload.get_payload()+"</td></tr>"
+                            body_string += "<tr><td>"+payload.get_payload()+"</td></tr>"
                 else:
                     for subpayload in payload.get_payload():
                         if not subpayload.is_multipart():
                             if 'Content-Type' in subpayload and ('text/plain' in subpayload['Content-Type'] or 'message/delivery-status' in subpayload['Content-Type'] or 'message/rfc822' in subpayload['Content-Type']):
-                                body_string = body_string + "<tr><td>"+subpayload.get_payload()+"</td></tr>"
+                                body_string += "<tr><td>"+subpayload.get_payload()+"</td></tr>"
                         else:
                             for subsubpayload in subpayload.get_payload():
                                 if not subsubpayload.is_multipart():
                                     if 'Content-Type' in subsubpayload and ('text/plain' in subsubpayload['Content-Type'] or 'message/delivery-status' in subsubpayload['Content-Type'] or 'message/rfc822' in subsubpayload['Content-Type']):
-                                        body_string = body_string + "<tr><td>"+subsubpayload.get_payload()+"</td></tr>"
+                                        body_string += "<tr><td>"+subsubpayload.get_payload()+"</td></tr>"
                                 else:
                                     pass
         else:
 
-            body_string = body_string + "<tr><td>"+em.get_payload()+"</td></tr>"
+            body_string += "<tr><td>"+em.get_payload()+"</td></tr>"
 
-        email_string = email_string+attachment_string
+        email_string += attachment_string
 
-        email_string = email_string+body_string
+        email_string += body_string
 
-        email_string = email_string+"</table>"
+        email_string += "</table>"
         
         secrets_file=open("/home/ec2-user/secrets.txt")
 
