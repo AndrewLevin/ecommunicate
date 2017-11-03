3b9d0fe6ebf81eff71a48fe61be74df00a55c30d
EC2 Default User <ec2-user@ip-10-0-0-117.us-west-2.compute.internal>
2017-11-03 15:05:32 +0000

    no commit message.

--- root.py
+++ root.py
@@ -56,20 +56,17 @@ class Root(object):
 Ecommunicate
 </title>
 <style>
-li.menubar {
-        display: inline;
-        padding: 20px;
-}
+
+.nonheader { width:960px; margin: 140px auto 0px auto;  }
+
+"""+html_strings.not_authenticated_header_style+"""
 </style>
 </head>
 <body>
-"""
+"""+html_strings.not_authenticated_header
     
-        html_string += "<center><h1>Ecommunicate</h1>"
-        html_string += "<h3>A free online communication service</h3>"
-        html_string += (html_strings.authenticated_menubar_html_string if utils.is_session_authenticated() else html_strings.not_authenticated_menubar_html_string)+"""
-    </center>
-"""
+        html_string += "<div class=\"nonheader\">"
+
         html_string += "Ecommunicate is a free online communication service in which all communication is viewable by anyone on the open internet instead of being private. Text messaging (like Google Hangouts or WeChat) and e-mail (to other ecommunicate.ch e-mail addresses) are implemented already, and we hope to eventually add audio and video calling (like Skype). You can chat or e-mail yourself (after registering and logging in) or you can view other people's chat conversations or e-mail inboxes (see below). This website is experimental at this point. You should expect bugs, unexpected downtime, etc. Please contact ecommunicate.feedback@gmail.com for comments, feature requests, etc.<br>"
 
         html_string += "<br>"
@@ -137,6 +134,8 @@ li.menubar {
         html_string += "</tr>\n"
 
         html_string += "</table>"
+
+        html_string += "</div>"
 
         html_string +=  """
 </body>
