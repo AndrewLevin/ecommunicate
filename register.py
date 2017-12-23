bf1d584056b2aa5c8906012fd8debb8d8df155d3
EC2 Default User <ec2-user@ip-10-0-0-117.us-west-2.compute.internal>
2017-12-23 01:05:36 +0000

    no commit message

--- register.py
+++ register.py
@@ -14,28 +14,122 @@ import utils
 
 import json
 
+#"""+(html_strings.authenticated_header if utils.is_session_authenticated() else html_strings.not_authenticated_header)+"""
+
 class Register(object):
     @cherrypy.expose
     def index(self):
-        return """<html>
+
+        is_mobile = False
+
+        if "Android" in cherrypy.request.headers['User-Agent'] or "iPhone" in cherrypy.request.headers['User-Agent'] or "iPad" in cherrypy.request.headers['User-Agent']:
+            is_mobile = True
+
+        if is_mobile:
+
+            return """<html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>
 Ecommunicate
 </title>
 <style>
 
-.nonheader { width:960px; margin: 80px auto 0px auto;  } 
+nav a, button {
+    min-width: 48px;
+    min-height: 48px;
+}
+
+
+html, body {
+    height: 100%;
+    width: 100%;
+    margin-top:0;
+    margin-left:0;
+    margin-right:0;
+}
+      
+a#menu svg {
+    width: 40px;
+    fill: #000;
+}
+
+main {
+    width: 100%;
+    height: 100%;
+}
+
+nav {
+    width: 250px;
+    height: 100%;
+    position: fixed;
+    transform: translate(-250px, 0);
+    transition: transform 0.3s ease;
+}
+
+nav.open {
+    transform: translate(0, 0);
+}
 
 .terminal {
-border: none; 
-width: 400px;
+   border: none; 
+   width: 100%;
 }
-"""+html_strings.header_style+"""
+
+.header {
+float : right
+
+}
+
+.content {
+padding-left:1em;
+padding-right:1em;
+
+}
+
+input[type="text"] {
+font-size: 120%;
+border-radius: 5px;
+border: 2px solid black;
+padding: 2px;
+outline: none;}
+
+input[type="password"] {
+font-size: 120%;
+border-radius: 5px;
+border: 2px solid black;
+padding: 2px;
+outline: none;}
+
 </style>
 </head>
 <body>
-"""+(html_strings.authenticated_header if utils.is_session_authenticated() else html_strings.not_authenticated_header)+"""
-<div class="nonheader">
+
+<nav id="drawer" style="background-color:LightGrey">
+<center><h2 style="margin-top:0">Ecommunicate</h2></center>
+<ul style="list-style:none;font-size:20px;padding-left:40px;">
+<li style="padding-bottom:20px"><a href="/">Home</a></li>
+<li style="padding-bottom:20px"><a href="/view/">View</a></li>
+<li style="padding-bottom:20px"><a href="/register/">Register</a></li>
+<li style="padding-bottom:20px"><a href="/loginlogout/login/">Login</a></li>
+<li><a href="/about">About</a></li>
+</ul>
+</nav>
+
+<main>
+<div style = "width:100%;top:0;left:0;">
+<a id="menu">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z" />
+  </svg>
+</a>
+<div class = "header">
+<h1 style="margin-top:0;margin-bottom:0">Ecommunicate</h1>
+</div>
+</div>
+
+<div class = "content">
+
 <center>
 <h2>Registration</h2>
 </center>
@@ -44,28 +138,49 @@ width: 400px;
 <br><br>
 <center>
    <form id="register_form" target="console_iframe" method="post" action="register">
-   username: * <br><br>
+   <div style = "font-size:120%">username: *</div><br>
    <input type="text" id="username" name="username" size="18" /><br><br>
-   password: * <br><br>
+   <div style = "font-size:120%">password: *</div> <br>
    <input type="password" id="password" name="password" size="18" /> <br><br>
-   name: <br><br>
+   <div style = "font-size:120%">name:</div> <br>
    <input type="text" id="name" name="name" size="18" /><br><br>
 
 <b>By clicking the "Register" button, you agree that you have read and understand the above description of what Ecommunicate is.</b> 
 
 <br> <br>
 
-  <button id="register" type="submit">
+  <button id="register" type="submit" style="font-size: 16px;">
   Register
   </button>
   </form>
   <iframe name="console_iframe" id ="console_iframe" class="terminal" /></iframe>
 </center>
+</div>
 
+</main>
 
   
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.0.js"></script>
   <script type="text/javascript">
+
+
+var menu = document.querySelector('#menu');
+var main = document.querySelector('main');
+var drawer = document.querySelector('#drawer');
+
+menu.addEventListener('click', function(e) {
+    drawer.classList.toggle('open');
+    e.stopPropagation();
+});
+  
+main.addEventListener('click', function() {
+    drawer.classList.remove('open');
+});
+
+main.addEventListener('touchstart', function() {
+    drawer.classList.remove('open');
+});
+
 
 $('#register_form').submit(function(event) {
 
@@ -118,14 +233,101 @@ $('#register_form').submit(function(event) {
 
 </script>        
   
+  </center>
+</body>
+        </html>"""
+
+        else:
+
+            return """<html>
+<head>
+<title>
+Ecommunicate
+</title>
+<style>
+
+.nonheader { width:960px; margin: 80px auto 0px auto;  } 
+
+.terminal {
+border: none; 
+width: 400px;
+}
+"""+html_strings.header_style+"""
+</style>
+</head>
+<body>
+"""+(html_strings.authenticated_header if utils.is_session_authenticated() else html_strings.not_authenticated_header)+"""
+<div class="nonheader">
+<center>
+<h2>Registration</h2>
+</center>
+      Ecommunicate is a free online communication service in which <b>all communication is viewable by anyone on the open internet</b> instead of being private. E-mail to other ecommunicate.ch e-mail addresses and text messaging (like Google Hangouts or WeChat) are implemented already, and we hope to eventually add audio and video calling (like Skype). You can chat or e-mail yourself (after registering and logging in) or you can view other people's chat conversations or e-mail inboxes. This website is experimental at this point. You should expect bugs, unexpected downtime, etc. Please contact ecommunicate.feedback@gmail.com for comments, feature requests, etc.<br> <br>
+      Register here for your free account. Please remember your username and password, as there is no way to recover them at this time.
+<br><br>
+<center>
+   <form id="register_form" target="console_iframe" method="post" action="register">
+   username: * <br><br>
+   <input type="text" id="username" name="username" size="18" /><br><br>
+   password: * <br><br>
+   <input type="password" id="password" name="password" size="18" /> <br><br>
+   name: <br><br>
+   <input type="text" id="name" name="name" size="18" /><br><br>
+
+<b>By clicking the "Register" button, you agree that you have read and understand the above description of what Ecommunicate is.</b> 
+
+<br> <br>
+
+  <button id="register" type="submit">
+  Register
+  </button>
+  </form>
+  <iframe name="console_iframe" id ="console_iframe" class="terminal" /></iframe>
+</center>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.0.js"></script>
+  <script type="text/javascript">
+$('#register_form').submit(function(event) {
+   event.preventDefault();
+   var $this = $(this);
+   $.ajax({
+      url: $this.attr('action'),
+      type: 'POST',
+      data: $this.serialize(),
+      success: function(data){
+        json_object = JSON.parse(data)
+        if (json_object["success"]) {
+            var console_iframe = document.getElementById('console_iframe');
+            console_iframe.contentWindow.document.open();
+            console_iframe.contentWindow.document.close();
+            $('#register_form').hide();
+            console_iframe.contentWindow.document.write('<center style="color:blue;font-size:20px;font-weight:bold">Registration was successful.</center>');
+        }
+        else {
+            var console_iframe = document.getElementById('console_iframe');
+            console_iframe.contentWindow.document.open();
+            console_iframe.contentWindow.document.close();
+            console_iframe.contentWindow.document.write('<center style="color:red;font-size:20px;font-weight:bold">'+json_object["errors"]+'</center>');
+        }
+      },
+      error : function (data) {
+        var console_iframe = document.getElementById('console_iframe');
+        console_iframe.write("Error.");
+      }
+   });
+});
+</script>        
   <br>
   <br>
   </center>
 </div>
 </body>
-        </html>"""
+</html>"""
+
+
+
     @cherrypy.expose
     def register(self, username, password,name):
+
 
         print "username: "+username
         print "len(password): "+str(len(password))
