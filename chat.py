b91d062dc8458d5106f647f0697dc484dcbf5af6
EC2 Default User <ec2-user@ip-10-0-0-117.us-west-2.compute.internal>
2017-12-29 02:03:16 +0000

    no commit message

--- chat.py
+++ chat.py
@@ -26,6 +26,11 @@ class Chat(object):
     @require()
     def index(self):
 
+        is_mobile = False
+
+        if "Android" in cherrypy.request.headers['User-Agent'] or "iPhone" in cherrypy.request.headers['User-Agent'] or "iPad" in cherrypy.request.headers['User-Agent']:
+            is_mobile = True
+
         secrets_file=open("/home/ec2-user/secrets.txt")
 
         passwords=secrets_file.read().rstrip('\n')
@@ -54,11 +59,15 @@ class Chat(object):
 
         iframes_string = "";
 
-        iframes_string += "<iframe id=\"console_"+cherrypy.session.get('_cp_username')+"\" name=\"console_"+cherrypy.session.get('_cp_username')+"\" class=\"terminal\" />  </iframe>\n"
+        iframes_string += "<iframe id=\"console_"+cherrypy.session.get('_cp_username')+"\" name=\"console_"+cherrypy.session.get('_cp_username')+"\" class=\"terminal\" src = \"about:blank\"/>  </iframe>\n"
 
         iframes_hide_string = "";
 
         iframes_hide_string += "$(\'#console_" + cherrypy.session.get('_cp_username') + "\').hide();\n"
+
+        iframes_touch_string = "var console_" + cherrypy.session.get('_cp_username') + " = document.getElementById('console_" + cherrypy.session.get('_cp_username') + "');\n"
+        
+        iframes_touch_string += "console_" + cherrypy.session.get('_cp_username') + ".addEventListener('touchstart', function(e) { drawer.classList.remove('open'); });\n"
 
         for contact in contacts:
 
@@ -78,13 +87,382 @@ class Chat(object):
 
             iframes_hide_string += "$(\'#console_" + username + "\').hide();\n"
 
+
+
         contacts_string += "</ul></td>\n</td>"
 
         click_on_self_string = "$(\'#"+cherrypy.session.get('_cp_username')+"\').click();"
 
         conn.close()
 
-        return """<html>
+        if is_mobile:
+
+            html_string = """
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>
+Ecommunicate
+</title>
+<style>
+
+nav a, button {
+    min-width: 48px;
+    min-height: 48px;
+}
+
+html, body {
+    height: 100%;
+    width: 100%;
+    margin-top:0;
+    margin-left:0;
+    margin-right:0;
+}
+
+a#menu svg {
+    width: 40px;
+    fill: #000;
+}
+
+main {
+    width: 100%;
+    height: 100%;
+}
+
+nav {
+    width: 250px;
+    height: 100%;
+    position: fixed;
+    transform: translate(-250px, 0);
+    transition: transform 0.3s ease;
+}
+
+nav.open {
+    transform: translate(0, 0);
+}
+
+.header {
+    float : right
+}
+
+.content {
+    padding-left:1em;
+    padding-right:1em;
+}
+
+ul.contactlistclass {
+    list-style:none;
+    padding-left:0;
+    width:100%;
+}
+    .contact {
+        color : black;
+        background-color: green;
+        height: 50px;
+        font-size: 24px;
+        width: 98.5%;
+        padding-top: 20px;
+        margin-top: 2px;
+        font-family: Verdana;
+        font-weight: bold;
+        cursor: default;
+        border-top: 2px solid black;
+        border-bottom: 2px solid black;
+        white-space:nowrap;
+        overflow:hidden;
+    }
+    .terminal {
+    width: 100%;
+    height: 30em;
+    border: none;
+}
+    .messageerrorbox {
+    width: 100%;
+    height: 1em;
+    border: none;
+}
+
+</style>
+
+</head>
+
+<body>
+
+<nav id="drawer" style="background-color:LightGrey">
+
+<center><h2 style="margin-top:0">Ecommunicate</h2></center>"""+(html_strings.authenticated_mobile_navigation_menu if utils.is_session_authenticated() else html_strings.not_authenticated_mobile_navigation_menu)+"""
+
+</nav>
+
+<main>
+
+<div style = "width:100%;top:0;left:0;">
+
+<a id="menu">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z" />
+  </svg>
+</a>
+<div class = "header">
+<h1 style="margin-top:0;margin-bottom:0">Ecommunicate</h1>
+</div>
+</div>
+<div class = "content">
+
+<table border = "0" style="float:left;padding:0;border-spacing:0;font-family:verdana;" width = "100%">
+<tr> <td style="padding:0;background-color:green">
+<table border="2"  id="makecontactrequeststable" width = "100%"><tr> <td width="100%" style="font-weight:bold"><center>Make Contact Requests</center></td></tr>
+</table>
+</td>
+</tr>
+<tr>
+</tr>
+<td>
+</td>
+<tr>
+<td style="padding:0;background-color:green">
+<table border="2" id = "respondtocontactrequeststable" width="100%">  <tr> <td width="100%" style="font-weight:bold"><center>Respond to Contact Requests</center></td></tr>
+</table>
+</td>
+</tr>
+</table>
+
+  <table border=2 width = "100%">
+  <tr>
+  <td width = "100%">
+"""+ iframes_string + """
+<form id="add_message_form" name = "add_messages_form" target="console_iframe1" method="POST" action="add_message" width = "100%">
+ <input type="text" id="add_message_text" name="add_message_text" style="width:100%;border:2px solid black;font-size: 120%;outline: none;" /> <br> <br>
+ <input type="hidden" id="username2" name="username2"/>
+ </form>
+ <iframe id="console_iframe1" name="console_iframe1" class="messageerrorbox" />  </iframe>
+</td>
+  </tr>
+  </table>
+
+  <table border=2 width = "100%">
+  <tr>
+""" + contacts_string + """ 
+  </tr>
+</table>
+
+</div>
+</main>
+
+
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.0.js"></script> 
+
+<script type="text/javascript">
+
+var menu = document.querySelector('#menu');
+
+var main = document.querySelector('main');
+
+var drawer = document.querySelector('#drawer');
+
+menu.addEventListener('click', function(e) {
+    drawer.classList.toggle('open');
+    e.stopPropagation();
+});
+
+main.addEventListener('click', function() {
+    drawer.classList.remove('open');
+});
+
+main.addEventListener('touchstart', function() {
+    drawer.classList.remove('open');
+});
+
+messages_json = "";
+messages_json_old = "";
+username2  = "";
+max_time = ""
+function show_messages(e){
+   var target = e.target;
+   if( target.id != "contactslist"){
+    var console_iframe2 = document.getElementById('console_'+e.target.id);
+    if (username2 != ""){
+    
+        $('#console_'+username2).hide();
+    }
+//    console_iframe2.slideDown('slow');
+ 
+    username2=e.target.id;
+    $('#console_'+username2).show();
+   }
+}
+function update_messages(){
+
+    if (messages_json != "") {
+        for (var item in messages_json) {  
+            var console_iframe2 = document.getElementById('console_'+item);
+
+            //clear the iframe
+            console_iframe2.contentWindow.document.open();
+            console_iframe2.contentWindow.document.close();
+            for ( var i = 0, l = messages_json[item].length; i < l; i++ ) {
+                console_iframe2.contentWindow.document.write(messages_json[item][i][0]+": "+messages_json[item][i][1]);
+                console_iframe2.contentWindow.document.write("<br>");
+            }
+
+            var console_iframe2_contentWindow_document = console_iframe2.contentWindow.document;
+            //this will "over-scroll", but it works i.e. it moves to the bottom    
+            $(console_iframe2_contentWindow_document).scrollTop($(console_iframe2_contentWindow_document).height());  
+
+
+            //writing to the document clears all of the event listeners that were already attached to it, so you need to reattach them
+            $('#console_'+item).contents().on('touchstart', function(event) { 
+                event.preventDefault();
+                drawer.classList.remove('open');
+                } 
+            );
+
+
+
+        }
+    }
+}
+function chat_initial() {
+   $.ajax({
+      url: 'get_messages',
+      type: 'GET',
+      dataType: 'html',
+      success: function(data) {
+         parsed_data = JSON.parse(data);
+         messages_json = parsed_data[0];
+         max_time=parsed_data[1]; 
+         messages_json_old = messages_json;
+         update_messages(); 
+         chat_recursive();
+      }
+   });
+}
+function chat_recursive() {
+get_messages_url = 'get_messages?upon_update=True&client_max_time='+max_time;
+   $.ajax({
+      url: get_messages_url,
+      type: 'GET',
+      dataType: 'html',
+      success: function(data) {
+         parsed_data = JSON.parse(data);
+         //alert(parsed_data["phone8"]);
+         messages_json = parsed_data[0];
+         max_time = parsed_data[1];
+         if (messages_json_old != ""){
+             for (var item in messages_json) {
+                 if (item in messages_json_old){
+                    if ( messages_json[item].length > messages_json_old[item].length ) {
+                        for ( var i = messages_json_old[item].length, l = messages_json[item].length; i < l; i++ ) { 
+                            if (messages_json[item][i][0] == item && item != username2){
+                                $('#'+item).css('background-color','blue');
+                            }
+                        }  
+                     }
+                 } else {
+                    for ( var i = 0, l = messages_json[item].length; i < l; i++ ) { 
+                        if (messages_json[item][i][0] == item && item != username2){
+                                $('#'+item).css('background-color','blue');
+                        }
+                    }  
+                 }
+             }
+         }
+         messages_json_old = messages_json;
+         update_messages();  
+         chat_recursive();
+      }
+   });
+}
+$('#add_message_form').submit(function(event) {
+   event.preventDefault();
+   $('input[name="username2"]',this).val(username2);
+   var $this = $(this);
+   //$.get($this.attr('action'),$this.serialize());
+   //alert($this.serialize());
+   $.ajax({
+      url: $this.attr('action'),
+      type: 'POST',
+      dataType: 'html',
+      data: $this.serialize()
+   
+   });
+   add_message_form.reset();
+});
+//$('#add_message_text')
+//.keyup(function(event) {
+//if (event.keyCode == 13){
+//alert(event.keyCode);
+//}
+//});
+var makecontactrequeststable = document.getElementById('makecontactrequeststable');
+
+makecontactrequeststable.addEventListener('mouseover',function(e) { $('#makecontactrequeststable').css('background-color','orange');  } ,  false);
+
+makecontactrequeststable.addEventListener('mouseout',function(e) { $('#makecontactrequeststable').css('background-color','green');  } ,  false);
+
+makecontactrequeststable.addEventListener('click',function(e) { open('https://ecommunicate.ch/chat/makecontactrequests/','_self')  } ,  false);
+
+var respondtocontactrequeststable = document.getElementById('respondtocontactrequeststable');
+
+respondtocontactrequeststable.addEventListener('mouseover',function(e) { $('#respondtocontactrequeststable').css('background-color','orange');  } ,  false);
+
+respondtocontactrequeststable.addEventListener('mouseout',function(e) { $('#respondtocontactrequeststable').css('background-color','green');  } ,  false);
+
+respondtocontactrequeststable.addEventListener('click',function(e) { open('https://ecommunicate.ch/chat/respondtocontactrequests/','_self') } ,  false);
+
+function contact_mouseover(e){
+var target = e.target;
+if( target.id != "contactslist"){
+    $('#'+target.id).css('background-color','orange');
+}
+
+}
+function contact_mouseout(e){
+var target = e.target;
+if( target.id != "contactslist"){
+    $('#'+target.id).css('background-color','green');
+}
+}
+console_iframe1=document.getElementById('console_iframe1')
+//var add_message_form = document.getElementById('add_message_form')
+//var message = document.getElementById('message')
+//add_message_form.addEventListener('submit', function (e) { 
+//e.preventDefault();
+//add_message_form.submit();
+//}, false)
+//console_iframe1.addEventListener('load', function(e) {
+//add_message_form.reset();
+//}, false)
+var contactslist = document.getElementById('contactslist')
+contactslist.addEventListener('touchstart', function(e) { show_messages(e); } , false )
+contactslist.addEventListener('click', function(e) { 
+    show_messages(e); 
+} , false )
+contactslist.addEventListener('mouseover',function(e) {contact_mouseover(e); } ,  false)
+contactslist.addEventListener('mouseout',function(e) {contact_mouseout(e); } ,  false)
+
+"""+iframes_touch_string+"""
+
+$(document).ready(function() {
+"""+iframes_hide_string+"""
+
+var console_iframe_var = document.getElementById('console_test1');
+
+chat_initial();
+
+"""+click_on_self_string+"""
+});
+
+</script>  
+
+</body></html>
+
+"""
+
+        else:
+
+            html_string = """<html>
 <head><title>Ecommunicate</title>
 <style>
 
@@ -189,6 +567,7 @@ function show_messages(e){
    }
 }
 function update_messages(){
+
     if (messages_json != "") {
         for (var item in messages_json) {  
             var console_iframe2 = document.getElementById('console_'+item);
@@ -199,9 +578,13 @@ function update_messages(){
                 console_iframe2.contentWindow.document.write(messages_json[item][i][0]+": "+messages_json[item][i][1]);
                 console_iframe2.contentWindow.document.write("<br>");
             }
+
             var console_iframe2_contentWindow_document = console_iframe2.contentWindow.document;
             //this will "over-scroll", but it works i.e. it moves to the bottom    
             $(console_iframe2_contentWindow_document).scrollTop($(console_iframe2_contentWindow_document).height());  
+
+
+
         }
     }
 }
@@ -330,6 +713,8 @@ $(document).ready(function() {
 });
 </script>
         </html>"""
+
+        return html_string
 
     @cherrypy.expose
     @require()
