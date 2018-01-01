8e5abc9beabf06bb5e3336720f5188453afaa6e0
EC2 Default User <ec2-user@ip-10-0-0-117.us-west-2.compute.internal>
2018-01-01 01:00:10 +0000

    no commit message

--- viewreadone.py
+++ viewreadone.py
@@ -118,8 +118,145 @@ class ViewReadOne(object):
         email_string += body_string
 
         email_string += "</table>"
+
+        is_mobile = False
+
+        if "User-Agent" in cherrypy.request.headers and ("Android" in cherrypy.request.headers['User-Agent'] or "iPhone" in cherrypy.request.headers['User-Agent'] or "iPad" in cherrypy.request.headers['User-Agent']):
+            is_mobile = True
+
+        if is_mobile:
         
-        return """<html>
+            html_string = """
+<head>
+
+"""+html_strings.google_adsense_conversion_tracking_global_site_tag+"""
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+"""+html_strings.google_adsense_conversion_tracking_global_site_tag+"""
+
+<title>
+Ecommunicate
+</title>
+
+<style>
+
+nav a, button {
+    min-width: 48px;
+    min-height: 48px;
+}
+
+html, body {
+    height: 100%;
+    width: 100%;
+    margin-top:0;
+    margin-left:0;
+    margin-right:0;
+}
+
+a#menu svg {
+    width: 40px;
+    fill: #000;
+}
+
+main {
+    width: 100%;
+    height: 100%;
+}
+
+nav {
+    width: 250px;
+    height: 100%;
+    position: fixed;
+    transform: translate(-250px, 0);
+    transition: transform 0.3s ease;
+}
+
+nav.open {
+    transform: translate(0, 0);
+}
+
+.header {
+    float : right
+}
+
+.content {
+    padding-left:1em;
+    padding-right:1em;
+}
+
+</style>
+
+</head>
+
+<body>
+
+<nav id="drawer" style="background-color:LightGrey">
+
+<center><h2 style="margin-top:0">Ecommunicate</h2></center>"""+(html_strings.authenticated_mobile_navigation_menu if utils.is_session_authenticated() else html_strings.not_authenticated_mobile_navigation_menu)+"""
+
+</nav>
+
+<main>
+
+<div style = "width:100%;top:0;left:0;">
+
+<a id="menu">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <path d="M2 6h20v3H2zm0 5h20v3H2zm0 5h20v3H2z" />
+  </svg>
+</a>
+
+<div class = "header">
+
+<h1 style="margin-top:0;margin-bottom:0">Ecommunicate</h1>
+
+</div>
+
+</div>
+
+<div class = "content">
+
+"""+email_string+"""
+
+</div>
+
+</main>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.0.js"></script>
+
+<script type="text/javascript">
+
+var menu = document.querySelector('#menu');
+
+var main = document.querySelector('main');
+
+var drawer = document.querySelector('#drawer');
+
+menu.addEventListener('click', function(e) {
+    drawer.classList.toggle('open');
+    e.stopPropagation();
+});
+
+main.addEventListener('click', function() {
+    drawer.classList.remove('open');
+});
+
+main.addEventListener('touchstart', function() {
+    drawer.classList.remove('open');
+});
+
+</script>
+
+</body>
+
+</html>
+
+"""
+
+        else:
+            
+            html_string = """<html>
 <head>
 
 """+html_strings.google_adsense_conversion_tracking_global_site_tag+"""
@@ -153,3 +290,6 @@ border: none;
 <script>
 </script>
         </html>"""
+
+
+        return html_string
