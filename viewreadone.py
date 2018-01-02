928bf81ba6be4aa21f2e59743f5ef0a717ee8ab7
EC2 Default User <ec2-user@ip-10-0-0-117.us-west-2.compute.internal>
2018-01-02 20:20:06 +0000

    no commit message

--- viewreadone.py
+++ viewreadone.py
@@ -133,8 +133,6 @@ class ViewReadOne(object):
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-"""+html_strings.google_adsense_conversion_tracking_global_site_tag+"""
-
 <title>
 Ecommunicate
 </title>
